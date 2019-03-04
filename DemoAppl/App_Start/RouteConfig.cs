680a41a5a37e99625c854abc2b93f095a8ea98e8
Sai <Sai@AZTaskVM1>
2019-03-04 18:23:54 +0000

    Setting the default page

--- DemoAppl/App_Start/RouteConfig.cs
+++ DemoAppl/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DemoAppl
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "FizzBuzz", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
